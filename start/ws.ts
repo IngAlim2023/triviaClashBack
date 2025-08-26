cc6e054f4ec4a557cf2840f5783e6ed5f04aa9f6
Jorge Porras <jorgeeporrassandoval@gmail.com>
2025-08-25 19:54:05 -0500

    actualizacion de jugadores con socket.io

--- start/ws.ts
+++ start/ws.ts
@@ -2,13 +2,31 @@ import app from '@adonisjs/core/services/app'
 import { Server } from 'socket.io'
 import server from '@adonisjs/core/services/server'
 
+let onlineGamer: string[] = []
+
 app.ready(() => {
   const io = new Server(server.getNodeServer(), {
     cors: {
-      origin: '*',
+      origin: '*'
     },
   })
   io.on('connection', (socket) => {
-    console.log('new conec', socket.id)
+    
+    socket.on('newUser', (username: string) => {
+      console.log(`Nuevo jugador: ${username}`)
+
+      // Guardar en lista (solo si no existe ya)
+      if (!onlineGamer.includes(username)) {
+        onlineGamer.push(username)
+      }
+
+      // Emitir a todos los clientes la lista actualizada
+      io.emit('newUser', onlineGamer)
+    })
+    
+    // Detectar desconexión
+    socket.on('disconnect', () => {
+      console.log('❌ Cliente desconectado:', socket.id)
+    })
   })
 })
